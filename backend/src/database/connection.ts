28716ce1b9adac25d8284c79a7194d97c5f1e539
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-03 17:58:17 +0100

    creating connetion file in knexfile
    adding files into our BD

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -4,8 +4,9 @@ import path from 'path';
 const connection = knex({
     client: 'sqlite3',
     connection:{
-        filename:path.resolve(__dirname, 'database.sqlite'),
+        filename: path.resolve(__dirname, 'database.sqlite'),
     },
+    useNullAsDefault: true,
 });
 
 export default connection;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,12 @@
-import express, { response, request } from 'express';
+import express from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
-
-//Request params: parametros que vem na propria rota que identificam um recurso
-//query param : parametros ue vem na propria rota geralmente opcionais para filtros , paginação
-// reqest body: parametros para criação /atualização de usuario
-
-const users =[
-    'Diego',
-    'Nelson',
-    'Robson',
-    'Erneto',
-    'Vanfffny'
-    ];
-
-app.get('/' , (request, response) =>{
-  
-    return response.json({message: 'Hello world'});
-});
-
+app.use(routes);
+app.use('/uploads',express.static(path.resolve(__dirname, '..','uploads')));
 
 
 app.listen(3333);
