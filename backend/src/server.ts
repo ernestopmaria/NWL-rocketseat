e2c0b5f65f098295d6ced8b46158f8a4e416d20d
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-03 07:08:40 +0100

    creating sqlite3 and routes files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,25 @@
-import express from 'express';
+import express, { response, request } from 'express';
 
 const app = express();
-app.get('/users' , (request, response) =>{
-    console.log('listagem de usuarios');
+app.use(express.json());
 
-    response.json([
+//Request params: parametros que vem na propria rota que identificam um recurso
+//query param : parametros ue vem na propria rota geralmente opcionais para filtros , paginação
+// reqest body: parametros para criação /atualização de usuario
+
+const users =[
     'Diego',
     'Nelson',
     'Robson',
     'Erneto',
     'Vanfffny'
-    ])
+    ];
+
+app.get('/' , (request, response) =>{
+  
+    return response.json({message: 'Hello world'});
 });
+
+
 
 app.listen(3333);
