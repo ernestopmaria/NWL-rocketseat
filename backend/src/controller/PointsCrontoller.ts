45e14ec93f74ec0c771e7aeb4e4be097a1232b16
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-04 20:07:19 +0100

    update

--- backend/src/controller/PointsCrontoller.ts
+++ backend/src/controller/PointsCrontoller.ts
@@ -3,6 +3,24 @@ import knex from '../database/connection';
 
 
 class PointsController {
+
+    async index (request:Request, response:Response){
+        //vamos fazer o filtro por query params
+        const {city, uf, items} =request.query;
+
+       const parsedItems = String(items)
+       .split(',')
+       .map(item=> Number(item.trim()));
+
+       const points = await knex('points')
+       .join('point_items' , 'points.id' , '=' , 'point_items.point_id')
+       .whereIn('point_items.item_id' , parsedItems)
+       .where('city' , String(city))
+       .where('uf' , String(uf))
+       .distinct()
+       .select('points.*');
+        return response.json({points});
+    }
     async show (request:Request, response:Response){
         const {id} = request.params;
         const point = await knex ('points').where('id' , id).first();
@@ -30,7 +48,7 @@ class PointsController {
         const trx = await knex.transaction();
 
         const point ={
-            image:'image-fake',
+            image:'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
                 name,
                 email,
                 whatsapp,
